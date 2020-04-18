841cc304098e9f64159ade9139406fb9120dbcea
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-04-17 21:03:49 -0400

    fixed if statement in main.html

--- wackywidgets/main_app/urls.py
+++ wackywidgets/main_app/urls.py
@@ -1,8 +1,9 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='home'),
-	path('add_widget/', views.add_widget, name='add_widget'),
-	path('<int:widget_id>/delete_widget/', views.delete_widget, name='delete_widget'),
+    path('', views.home, name='home'),
+    path('add_widget/', views.add_widget, name='add_widget'),
+    path('<int:widget_id>/delete/', views.delete_widget, name='delete_widget'),
 ]
--- wackywidgets/main_app/views.py
+++ wackywidgets/main_app/views.py
@@ -1,26 +1,24 @@
 from django.shortcuts import render, redirect
-from .models import Widget
+from .models import *
 from .forms import WidgetForm
 
 # Create your views here.
-
 def home(request):
-	widgets = Widget.objects.all()
-	widget_form = WidgetForm()
-	return render(request, 'main.html', { 
-		'widgets': widgets, 
-		'widget_form': widget_form 
+    widgets_list = Widget.objects.all()
+    widget_form = WidgetForm()
+    return render(request, 'main.html', {
+		'widget_list': widgets_list, 
+		'widget_form': widget_form
 		})
 
-
 def add_widget(request):
-	form = WidgetForm(request.POST)
-	if form.is_valid():
-		form.save()
-	return redirect('home')
-
+    form = WidgetForm(request.POST)
+    if form.is_valid():
+        new_widget = form.save(commit=False)
+        new_widget.save()
+    return redirect('home')
 
 def delete_widget(request, widget_id):
-	Widget.objects.get(id=widget_id).delete()
-	return redirect('home')
+    Widget.objects.filter(id=widget_id).delete()
+    return redirect('home')
 
